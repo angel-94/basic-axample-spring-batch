467b5ec65274d1c43404d9490f7e615cbc025e5c
Angel Ruiz <angel138ms@gmail.com>
2019-01-30 01:27:03 -0600

    Update readme and Dockerfile

--- Docker/build-docker.sh
+++ Docker/build-docker.sh
@@ -2,15 +2,19 @@
 cd ..
 gradle build -x test --stacktrace
 
-cd Doccker
+cd Docker
 rm -rf build
 mkdir build
 cp ../build/libs/processing-batch-0.0.1-SNAPSHOT.jar build
 cd Doccker
 
 
-docker stop batch-processing
-docker rm -vf batch-processing
-docker build -t mars/batch-processing:v$1 .
-docker run -d -p 9000:8080 --name batch-processing mars/batch-processing:v$1
+docker stop batch-example
+docker rm -vf batch-example
+
+# Creamos la imagen
+docker build -t mars/batch:v$1 .
+
+# Creamos el contenedor
+docker run -d -p 9000:8080 --name batch-example mars/batch:v$1
 docker ps -a
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # basic-axample-spring-batch
 Ejemplo básico con spring batch
+
+
+#### Ejecutar el ejemplo usando docker
+
+* Seguir los siguientes pasos
+  * Clonar el proyecto
+  * Acceder en el directorio del proyecto clonado ``processing-batch/Docker``
+  * Ejecutar el siguiente comando ``./build-docker.sh $version``
+  * Abrir un explorador con la siguiente [url](http://localhost:9000/swagger-ui.html#)
+  
